bb5f716f41d912e93d794529d9a3c87e4c12c2c2
xd009642 <danielmckenna93@gmail.com>
2023-11-08 18:52:12 +0000

    Start to prepare input data
    In demo app add dictionary, map to units. Move some of the speedyspeech
    stuff to phoneme module as it seems generally useful. Now take in a
    transcript and should generate a sequence of phonemes. Next step is to
    push it through the network and then get a spectrogram out

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,5 +1,33 @@
+use clap::Parser;
+use xd_tts::phonemes::*;
 use xd_tts::speedy_candle::*;
+use xd_tts::training::cmu_dict::*;
 
-fn main() {
-    let model = SpeedySpeech::load("./models/speedyspeech.onnx").unwrap();
+#[derive(Parser, Debug)]
+pub struct Args {
+    #[clap(long, short)]
+    input: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let dict = CmuDictionary::open("data/cmudict-0.7b.txt")?;
+    let model = SpeedySpeech::load("./models/speedyspeech.onnx")?;
+
+    let mut words = vec![];
+
+    for word in args.input.split_whitespace() {
+        if let Some(pronunciation) = dict.get_pronunciations(word) {
+            assert!(!pronunciation.is_empty());
+            words.extend(pronunciation[0].iter().map(|x| Unit::Phone(*x)));
+            words.push(Unit::Space);
+        } else {
+            println!("Unsupported word: '{}'", word);
+        }
+    }
+
+    let spectrogram = model.infer(&words)?;
+
+    Ok(())
 }
--- src/speedy_candle.rs
+++ src/speedy_candle.rs
@@ -2,25 +2,13 @@ use crate::phonemes::*;
 use candle_core::{DType, Device, Tensor};
 use candle_nn::VarBuilder;
 use candle_onnx::onnx::ModelProto;
+use ndarray::Array2;
 use std::collections::HashMap;
 use std::path::Path;
 use std::str::FromStr;
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd)]
-enum PhonemeId {
-    Pad,
-    Unk,
-    Phoneme(PhoneticUnit),
-    Space,
-    FullStop,
-    Comma,
-    QuestionMark,
-    ExclamationMark,
-    Dash,
-}
-
-fn generate_id_list() -> Vec<PhonemeId> {
-    let mut res = vec![PhonemeId::Pad, PhonemeId::Unk];
+fn generate_id_list() -> Vec<Unit> {
+    let mut res = vec![Unit::Padding, Unit::Unk];
 
     let phones = [
         "AA0", "AA1", "AA2", "AE0", "AE1", "AE2", "AH0", "AH1", "AH2", "AO0", "AO1", "AO2", "AW0",
@@ -30,14 +18,14 @@ fn generate_id_list() -> Vec<PhonemeId> {
         "T", "TH", "UH0", "UH1", "UH2", "UW", "UW0", "UW1", "UW2", "V", "W", "Y", "Z", "ZH",
     ];
 
-    res.extend(phones.map(|x| PhonemeId::Phoneme(PhoneticUnit::from_str(x).unwrap())));
+    res.extend(phones.map(|x| Unit::from_str(x).unwrap()));
     res.extend_from_slice(&[
-        PhonemeId::Space,
-        PhonemeId::FullStop,
-        PhonemeId::Comma,
-        PhonemeId::QuestionMark,
-        PhonemeId::ExclamationMark,
-        PhonemeId::Dash,
+        Unit::Space,
+        Unit::FullStop,
+        Unit::Comma,
+        Unit::QuestionMark,
+        Unit::ExclamationMark,
+        Unit::Dash,
     ]);
 
     res
@@ -45,7 +33,7 @@ fn generate_id_list() -> Vec<PhonemeId> {
 
 pub struct SpeedySpeech {
     model_proto: ModelProto,
-    phoneme_ids: Vec<PhonemeId>,
+    phoneme_ids: Vec<Unit>,
 }
 
 impl SpeedySpeech {
@@ -69,33 +57,47 @@ impl SpeedySpeech {
         })
     }
 
-    pub fn infer(&self) -> anyhow::Result<()> {
+    pub fn infer(&self, units: &[Unit]) -> anyhow::Result<Array2<f64>> {
+        let phonemes = units
+            .iter()
+            .map(|x| self.best_match_for_unit(x))
+            .collect::<Vec<_>>();
         let inputs = HashMap::new();
+
         candle_onnx::simple_eval(&self.model_proto, inputs)?;
         todo!()
     }
 
-    fn best_match_for_unit(&self, unit: PhoneticUnit) -> usize {
-        let mut best = 2; // UNK
-        for (i, potential) in self
-            .phoneme_ids
-            .iter()
-            .enumerate()
-            .filter(|(_, x)| matches!(x, PhonemeId::Phoneme(v) if v.phone == unit.phone))
-        {
-            if best == 2 {
-                best = i;
-            }
-            if let PhonemeId::Phoneme(v) = potential {
-                if unit.context.is_none() {
+    fn best_match_for_unit(&self, unit: &Unit) -> usize {
+        if let Unit::Phone(unit) = unit {
+            let mut best = 2; // UNK
+            for (i, potential) in self
+                .phoneme_ids
+                .iter()
+                .enumerate()
+                .filter(|(_, x)| matches!(x, Unit::Phone(v) if v.phone == unit.phone))
+            {
+                if best == 2 {
                     best = i;
-                    break;
-                } else if *v == unit {
-                    best = i;
-                    break;
+                }
+                if let Unit::Phone(v) = potential {
+                    if unit.context.is_none() {
+                        best = i;
+                        break;
+                    } else if v == unit {
+                        best = i;
+                        break;
+                    }
                 }
             }
+            best
+        } else {
+            self.phoneme_ids
+                .iter()
+                .enumerate()
+                .find(|(_, x)| *x == unit)
+                .map(|(i, _)| i)
+                .unwrap_or(2)
         }
-        best
     }
 }
