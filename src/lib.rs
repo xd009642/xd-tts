08fd39fb4117da34e2274f8095ec2647414d248a
xd009642 <danielmckenna93@gmail.com>
2023-11-17 19:29:32 +0000

    So here I fix some text norm issues and...
    Start to integrate tract. but oh no ONNX sequences aren't supported!
    
    I'll still merge it as either way I don't have a working inference in
    ONNX anywhere and this at least keeps the useful refactorings

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,14 @@ use tracing_subscriber::{Layer, Registry};
 
 pub mod infer;
 pub mod phonemes;
-pub mod speedy_candle;
+pub mod speedyspeech;
 pub mod text_normaliser;
 pub mod training;
 
 pub fn setup_logging() {
     let filter = match env::var("RUST_LOG") {
         Ok(_) => EnvFilter::from_env("RUST_LOG"),
-        _ => EnvFilter::new("xd-tts=info,app=info,trainer=info"),
+        _ => EnvFilter::new("xd_tts=info,app=info,trainer=info"),
     };
 
     let fmt = tracing_subscriber::fmt::Layer::default();
---
+++ src/speedyspeech/speedy_tract.rs
@@ -0,0 +1,47 @@
+use super::*;
+use anyhow::Context;
+use ndarray::Array2;
+use std::path::Path;
+use tract_onnx::prelude::*;
+use tract_onnx::tract_hir::infer::InferenceOp;
+
+pub struct SpeedyTract {
+    model:
+        SimplePlan<InferenceFact, Box<dyn InferenceOp>, Graph<InferenceFact, Box<dyn InferenceOp>>>,
+    phoneme_ids: Vec<Unit>,
+}
+
+impl SpeedyTract {
+    #[must_use]
+    pub fn load(path: impl AsRef<Path>) -> anyhow::Result<Self> {
+        let model = tract_onnx::onnx()
+            .model_for_path(path)
+            .context("loading ONNX file")?
+            // https://github.com/sonos/tract/issues/1263
+            //    .into_optimized()
+            //    .context("optimising graph")?
+            .into_runnable()
+            .context("converting to runnable model")?;
+
+        Ok(Self {
+            model,
+            phoneme_ids: generate_id_list(),
+        })
+    }
+
+    pub fn infer(&self, units: &[Unit]) -> anyhow::Result<Array2<f32>> {
+        let phonemes = units
+            .iter()
+            .map(|x| best_match_for_unit(x, &self.phoneme_ids))
+            .collect::<Vec<_>>();
+
+        let tensor = Tensor::from_shape(&[1, units.len()], &phonemes)?;
+        let plen = Tensor::from(units.len() as i64);
+
+        let result = self.model.run(tvec!(tensor.into(), plen.into()))?;
+
+        tracing::info!("Result: {:?}", result);
+
+        todo!()
+    }
+}
