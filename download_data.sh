58360d26961e9b29258a5c7d140c486b0e16d3e4
xd009642 <danielmckenna93@gmail.com>
2024-04-02 09:13:24 +0100

    Apply some fixes spotted

--- download_data.sh
+++ download_data.sh
@@ -2,4 +2,4 @@
 
 mkdir data
 
-curl https://raw.githubusercontent.com/Alexir/CMUdict/master/cmudict-0.7b --output data/cmudict-0.7b
+curl https://raw.githubusercontent.com/Alexir/CMUdict/master/cmudict-0.7b --output data/cmudict-0.7b.txt
